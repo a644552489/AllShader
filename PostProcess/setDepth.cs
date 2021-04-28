c790c3aed640cc22b3f89fe41cb2342baa328ac5
a644552489 <69066257+a644552489@users.noreply.github.com>
2021-04-28 15:05:59 +0800

    Add files via upload

---
+++ PostProcess/setDepth.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class setDepth : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        GetComponent<Camera>().depthTextureMode = DepthTextureMode.Depth;
+    }
+ 
+}
